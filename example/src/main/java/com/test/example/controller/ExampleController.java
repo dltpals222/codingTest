a8e9d3f0b32e28629b0afaa4805a13d880f18d3f
blahblah <leesemin222@gmail.com>
2023-07-26 14:20:44 +0900

    서버 띄우기 성공

--- example/src/main/java/com/test/example/controller/ExampleController.java
+++ example/src/main/java/com/test/example/controller/ExampleController.java
@@ -1,6 +1,15 @@
 package com.test.example.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ExampleController {
-  
+  @GetMapping(value = "/index")
+  public String home(Model model){
+    model.addAttribute("test", "테스트입니다.");
+    return "home";
+  }
+
 }
