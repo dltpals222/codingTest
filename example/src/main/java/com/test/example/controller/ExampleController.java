06258fa4378cedd21d9223a58114b89f1ed85fa5
blahblah <leesemin222@gmail.com>
2023-08-03 16:47:42 +0900

    views폴더 생성 후 이동

--- example/src/main/java/com/test/example/controller/ExampleController.java
+++ example/src/main/java/com/test/example/controller/ExampleController.java
@@ -1,15 +1,17 @@
 package com.test.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class ExampleController {
-  @GetMapping(value = "/index")
-  public String home(Model model){
-    model.addAttribute("test", "테스트입니다.");
-    return "home";
+
+  @GetMapping(value = "/")
+  public String showIndex(Model model){
+    model.addAttribute("message", "Hello World!");
+    return "index";
   }
 
 }
